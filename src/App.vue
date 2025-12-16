425600eef65efd1109ca27baf6e331365e24d817
Dmitriy <dima132345@gmail.com>
2025-12-16 16:15:45 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TaskItem from './components/TaskItem.vue'
+
+const tasks = [
+  {id: 1, title: "посуда", text: "сегодня нужно обязательно помыть посуду!"},
+  {id: 2, title: "уроки", text: "нужно сделать все уроки и долги по учебе"},
+  {id: 3, title: "собака", text: "нужно обязательно погулять с собакой"},
+  {id: 4, title: "купить продукты", text: "молоко, чай, яйца, хлеб"}
+]
+</script>
+
+<template>
+<h1>Мой список дел</h1>
+<TaskItem v-for = "task in tasks" :key="task.id" :task="task"/>  
+</template>
+
+
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{task:{
+    title: string;
+    text: string;
+}}>()
+</script>
+
+<template>
+    <div class="task-item">
+        <h3>{{ props.task.title }}</h3>
+        <p>{{ props.task.text }}</p>
+    </div>
+
+
+</template>
+
+<style>
+.task-item{
+    border: 2px solid #42b983;
+    border-radius: 10px;
+    padding: 15px;
+    margin-bottom: 15px;
+    background-color: #f9f9f9;
+}
+h3 {
+    margin-top: 0;
+    color: #2c3e50;
+}
+p{
+    color: #2c3e50; 
+}
+</style>
